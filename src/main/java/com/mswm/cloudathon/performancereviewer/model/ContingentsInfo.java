bc725985948786d3062889f3fde7db69fb43c10a
sahil426 <sahil426@gmail.com>
2022-02-28 10:51:51 +0000

    Initial Commit

---
+++ src/main/java/com/mswm/cloudathon/performancereviewer/model/ContingentsInfo.java
@@ -0,0 +1,17 @@
+package com.mswm.cloudathon.performancereviewer.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ContingentsInfo {
+    private String emailAddress;
+    private String fullName;
+    private String mobilePhone;
+    private String userId;
+    private String techFamily;
+    private String vendor;
+    private String reviewStatus;
+    private String reviewedOn;
+}
